5ace7ebc8e3e8749a3268bda2f274599204656ac
Christoffer Hjort <cfhj@novonordisk.com>
2025-12-11 19:52:11 +0000

    update covid19 analysis

--- models/bronze/stg_cases.sql
+++ models/bronze/stg_cases.sql
@@ -1,7 +1,7 @@
 select
+    iso3166_1 as country_code,
     country_region,
     cases,
     deaths,
     date
-
 from {{ source('source_covid19', 'ecdc_global') }}
--- models/silver/case_statistics.sql
+++
@@ -1,8 +0,0 @@
-select
-    country_region,
-    date,
-    cases,
-    deaths,
-    deaths / cases as cfr
-from  {{ ref('stg_cases') }}
-where cases > 0
